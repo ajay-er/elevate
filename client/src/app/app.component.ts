e6cdad6452818b6a5e84f91c7115e4d7b3421908
ajay <ajayzyan100@gmail.com>
2023-10-15 01:51:18 +0530

    basic header and footer template completed

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'client';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
