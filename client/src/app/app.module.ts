295825611dea4ca7cd8d9d9527429986a9364b69
ajay <ajayzyan100@gmail.com>
2023-10-18 15:44:18 +0530

    intial setup completed

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterModule } from './shared/ui/footer/footer.module';
+import { NavModule } from './shared/ui/nav/nav.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FooterModule,
+    NavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
